a16f78cd917d9fa6770eeae7370c77504188442f
bestlatte <x123456789xxxxxxtw@gmail.com>
2025-08-05 22:58:47 +0800

    新增ExceptionHandler(BadSqlGrammarException.class)

--- src/main/java/org/example/productshop/exception/GlobalExceptionHandler.java
+++ src/main/java/org/example/productshop/exception/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package org.example.productshop.exception;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,6 +15,15 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.CONFLICT)
                 .body("註冊失敗：email 已被註冊");
     }
+
+    @ExceptionHandler(BadSqlGrammarException.class)
+    public ResponseEntity<String> handleBadSql(BadSqlGrammarException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body("資料庫錯誤，可能是欄位不存在或SQL錯誤");
+    }
+
+
+
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleOtherErrors(Exception e) {
