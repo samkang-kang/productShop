8c5b5e336f9e4c20a094b17f47748bbece839c9c
bestlatte <x123456789xxxxxxtw@gmail.com>
2025-08-06 21:42:37 +0800

    新增登入功能 JWT功能

---
+++ src/main/java/org/example/productshop/controller/LoginController.java
@@ -0,0 +1,53 @@
+package org.example.productshop.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.constraints.Email;
+import org.example.productshop.dao.LoginDao;
+import org.example.productshop.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@RestController
+//@CrossOrigin(origins = "*")
+public class LoginController {
+
+    @Autowired
+    LoginService loginService;
+    @Autowired
+    LoginDao loginDao;
+
+    @PostMapping("/users/login")
+    public Map<String, Object> login(@RequestBody Map<String,String> request) {
+        String email = request.get("email");
+        String password = request.get("password");
+        String token = loginService.login(email, password);
+        HashMap<String, Object> response = new HashMap<>();
+
+        if(!"active".equals(loginDao.checkStatus(email))){
+             response.put("status", "fail");
+             return response ;
+        }
+
+        if(token != null) {
+            response.put("status", "success");
+            response.put("message", "登入成功");
+            response.put("token", token);
+        }else{
+            response.put("status", "fail");
+            response.put("message", "帳號或密碼錯誤");
+
+        }
+        return response;
+
+    }
+
+    @GetMapping("/secure/test")
+    public String testSecure(HttpServletRequest request) {
+        return "Hello, " + request.getAttribute("userEmail");
+    }
+
+}
