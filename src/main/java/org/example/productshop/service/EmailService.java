7602bb353810574f88cbd29e733f92abe81bea82
bestlatte <x123456789xxxxxxtw@gmail.com>
2025-08-08 17:10:29 +0800

    增加sendRestPasswordEmail方法

--- src/main/java/org/example/productshop/service/EmailService.java
+++ src/main/java/org/example/productshop/service/EmailService.java
@@ -23,4 +23,19 @@ public class EmailService {
 
         mailSender.send(message);
     }
+
+    public void sendRestPasswordEmail(String to, String token) {
+        String verifyUrl = "http://localhost:8080/users/password/reset/confirm?token=" + token;
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject("會員驗證信");
+        message.setText("請點擊以下連結完成帳號啟用：\n" + verifyUrl);
+
+        mailSender.send(message);
+    }
+
+
+
+
 }
